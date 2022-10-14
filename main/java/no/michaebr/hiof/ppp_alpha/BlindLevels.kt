b337c195e3133f8839a5db85c9e7d2a6d963cadd
mikethebik3 <100699135+mikethebik3@users.noreply.github.com>
2022-10-14 19:00:48 +0200

    Add files via upload

---
+++ main/java/no/michaebr/hiof/ppp_alpha/BlindLevels.kt
@@ -0,0 +1,47 @@
+package no.michaebr.hiof.ppp_alpha
+
+import android.os.Bundle
+import android.os.CountDownTimer
+import androidx.fragment.app.Fragment
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import androidx.appcompat.widget.AppCompatButton
+import androidx.appcompat.widget.AppCompatTextView
+import androidx.constraintlayout.widget.ConstraintLayout
+import androidx.navigation.fragment.findNavController
+import no.michaebr.hiof.ppp_alpha.databinding.BlindlevelsBinding
+
+class BlindLevels : Fragment() {
+
+    private lateinit var timer: CountDownTimer
+    private var _binding: BlindlevelsBinding? = null
+
+
+    private val binding get() = _binding!!
+
+    override fun onCreateView(
+        inflater: LayoutInflater, container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+
+        _binding = BlindlevelsBinding.inflate(inflater, container, false)
+        return binding.root
+
+    }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+
+        binding.buttonSecond.setOnClickListener {
+            findNavController().navigate(R.id.action_SecondFragment_to_FirstFragment)
+        }
+    }
+
+    override fun onDestroyView() {
+        super.onDestroyView()
+        _binding = null
+
+
+    }
+    }
